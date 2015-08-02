2793bc076ae6430ebee8bf791f3e5a0f31f4a045
meni <meni@gmail.com>
2015-08-02 15:46:13 +0300

    few changes

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,14 +1,32 @@
 /**
  * Created by meni on 02/08/15.
  */
+import java.util.Random;
 import java.util.function.DoubleBinaryOperator;
 
 public class Lambda {
+
+    public static final int TIMES = 1000;
+
+    public static double getNext() {
+        Random randomGenerator = new Random();
+        return  Math.log(1-randomGenerator.nextDouble())/(-2);
+    }
+
     public static double foo(DoubleBinaryOperator oper) {
         return oper.applyAsDouble(5,10);
     }
 
     public static void main(String[] args) {
         System.out.println(foo((x, y) -> x+y));
+
+        double sum = 0;
+
+        for(int i = 1; i < TIMES; i++){
+            sum += getNext();
+        }
+        double mean = sum / TIMES;
+
+        System.out.print("the mean is: " + mean);
     }
 }
